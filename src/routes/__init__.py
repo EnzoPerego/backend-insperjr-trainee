919d52f0d569ba6bd31d40b076db307d030a1db6
Enzo Perego <enzorp@al.insper.edu.br>
2025-10-01 11:45:41 -0300

    Rotas para Adicionar Produtos

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -6,11 +6,13 @@ from .produtos import router as produtos_router
 from .clientes import router as clientes_router
 from .auth import router as auth_router
 from .funcionarios import router as funcionarios_router
+from .files import router as files_router
 
 __all__ = [
     'categorias_router',
     'produtos_router', 
     'clientes_router',
     'auth_router',
-    'funcionarios_router'
+    'funcionarios_router',
+    'files_router'
 ]
